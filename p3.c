8d41a4fc9c0d6cd56258e93eae086e2ceecf20d1
YasminDelCury <42417728+YasminDelCury@users.noreply.github.com>
2018-10-16 13:00:36 -0300

    Update p3.c

--- p3.c
+++ p3.c
@@ -4,17 +4,43 @@
 
 #include <stdio.h>
 
+int potencia(int exp)
+{
+    int i=0, p=1;
+    while(i<exp)
+    {
+        p=p*10;
+        i++;
+    }
+    return p;
+}
+
+
 int main()
 {
-    int i=0;
+    int i=0, j=0, num=0, len=0, n=0;
     char bits[256];
+	int aux[256];
     printf("\nDigite um texto com numeros:");
     scanf("%s", bits);
     printf("\n");
     for(i=0; bits[i]!='\0'; i++)
     {
         if(bits[i]<='9' && bits[i]>='0')
-            printf("%c", bits[i]);
+        {
+        	aux[j]=bits[i]-'0';
+        	j++;
+		}
+            
     }
-    return 0;
+    aux[j]='\0';
+    j--;
+	while(j>=0)
+	{
+		num=num + (aux[j])*potencia(n);
+		n++;
+		j--;
+	}
+	printf("\nNumero: %d", num);
+	return 0;
 }
